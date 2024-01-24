5425355b32ac613c14b9f65f37d25ced49b8a7d0
Deultimate17 <adigunibrahim17@gmail.com>
2024-01-24 10:01:37 +0100

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,12 @@ import 'package:qiii_app/quiz_app.dart';
 void main() {
   runApp(
     MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark
+      ),
       home: QuizApp(),
     )
   );
